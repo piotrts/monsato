86f0bfe06fa87a09151b05cfd47e37e0d50c5cb3
Piotr Tarasewicz <tarasewiczp@gmail.com>
2017-05-12 00:31:59 +0200

    bump version number; change indent in project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject monsato "0.1.0-SNAPSHOT"
+(defproject monsato "0.1.0"
   :description "A lightweight MutationObserver wrapper for ClojureScript"
   :url "http://github.com/piotrts/monsato"
   :license {:name "Eclipse Public License"
@@ -9,22 +9,19 @@
   :plugins [[lein-cljsbuild "1.1.4" :exclusions [[org.clojure/clojure]]]]
   :prep-tasks ["compile" ["cljsbuild" "once"]]
   :clean-targets ^{:protect false} ["resources/public/js/compiled" "target"]
-  :cljsbuild
-    {:builds 
-      [{:id "dev"
-        :source-paths ["src"]
-        :jar true
-        :compiler
-          {:main monsato.core
-           :output-to "resources/public/js/compiled/monsato.js"
-           :output-dir "resources/public/js/compiled/out"
-           :optimizations :none
-           :source-map true
-           :pretty-print true}}
-      {:id "min"
-       :source-paths ["src"]
-       :compiler
-         {:output-to "resources/public/js/compiled/monsato.js"
-          :optimizations :advanced
-          :pretty-print false}}]}
+  :cljsbuild {:builds
+              [{:id "dev"
+                :source-paths ["src"]
+                :jar true
+                :compiler {:main monsato.core
+                           :output-to "resources/public/js/compiled/monsato.js"
+                           :output-dir "resources/public/js/compiled/out"
+                           :optimizations :none
+                           :source-map true
+                           :pretty-print true}}
+               {:id "min"
+                :source-paths ["src"]
+                :compiler {:output-to "resources/public/js/compiled/monsato.js"
+                           :optimizations :advanced
+                           :pretty-print false}}]}
   :source-paths ["src"])
